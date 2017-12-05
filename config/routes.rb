e7f056e5092b599e99b2e3bce216321cbfade649
guillaume schwaederle <guillaume.schwaederle@gmail.com>
2017-12-05 15:32:00 +0100

    Création controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,30 @@
 Rails.application.routes.draw do
+  get 'series/create'
+
+  get 'series/update'
+
+  get 'series/destroy'
+
+  get 'trainings/index'
+
+  get 'trainings/new'
+
+  get 'trainings/create'
+
+  get 'trainings/edit'
+
+  get 'trainings/update'
+
+  get 'profiles/new'
+
+  get 'profiles/create'
+
+  get 'profiles/show'
+
+  get 'profiles/edit'
+
+  get 'profiles/update'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
