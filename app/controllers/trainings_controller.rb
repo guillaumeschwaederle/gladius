ff0f51530be838f8b257e0dc4ec56fe3d63492e9
Arthur Thieffry <arthieffry@gmail.com>
2017-12-05 16:10:01 +0100

    routes v1 + controller training v1

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,16 +1,41 @@
 class TrainingsController < ApplicationController
+  before_action :set_training, only: [:edit, :update]
+
   def index
+    @trainings = Training.all
   end
 
   def new
+    @training = Training.new
   end
 
   def create
+    @training = Training.new(training_params)
+    if @training.save
+      redirect_to trainings_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @training.update(training_params)
+      redirect_to trainings_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_training
+    @training = Training.find(params[:id])
+  end
+
+  def training_params
+    params.require(:training).permit(:name, :goal, :profile_id)
   end
 end
