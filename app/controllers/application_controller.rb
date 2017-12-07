11aca1b0da54dfef7fc272485191a1cc4e731959
Claire Soullie <cmp.soullie@gmail.com>
2017-12-07 14:48:25 +0100

    add meta description et meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
     before_action :authenticate_user!
     before_action :profile_present?
+
+  def default_url_options
+    { host: ENV['HOST'] || 'localhost:3000' }
+  end
+
  private
 
    def profile_present?
