ab9b382e37e8e6353abcab1158c62da03331a153
Arthur Thieffry <arthieffry@gmail.com>
2017-12-08 13:01:26 +0100

    Statistiques

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -9,7 +9,9 @@ class TrainingsController < ApplicationController
     gon.series_training = @series_training
     gon.series_exercice = @series_exercice
     gon.exercices = @exercices
-
+    @ratio_total = ratio(@trainings)
+    @completion_total = completion(@trainings)
+    @total = number(@trainings)
   end
 
   def new
@@ -48,4 +50,31 @@ class TrainingsController < ApplicationController
     params.require(:training).permit(:name, :profile_id)
   end
 
+  def completion(arr)
+    average_arr = []
+    arr.select { |a| a.completion > 0 }.each do |training|
+      average_arr << training.completion
+    end
+    unless average_arr == []
+      average_arr.sum / average_arr.size
+    else
+      0
+    end
+  end
+
+  def ratio(arr)
+    arr.select { |a| a.completion > 0 }.size / arr.size.to_f * 100
+  end
+
+  def number(arr)
+    done = 0
+    goal = 0
+    arr.select { |a| a.completion > 0 }.each do |training|
+      training.series.each do |serie|
+          done += serie.done if serie.done
+          goal += serie.goal
+      end
+    end
+    {done: done, goal: goal}
+  end
 end
