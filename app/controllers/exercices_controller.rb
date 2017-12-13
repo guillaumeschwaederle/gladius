de06e1224edb25af3570f4211e6a5358cef2961e
Claire Soullie <cmp.soullie@gmail.com>
2017-12-13 18:36:03 +0100

    create exercice 1/2

--- app/controllers/exercices_controller.rb
+++ app/controllers/exercices_controller.rb
@@ -1,5 +1,42 @@
 class ExercicesController < ApplicationController
+  before_action :set_exercice, only: [ :edit, :update, :destroy]
+
   def index
+   @exercices = Exercice.all
   end
 
+  def new
+    @exercice = Exercice.new
+  end
+
+  def create
+    @exercice = Exercice.new(exercice_params)
+    @exercice.category = 'Exercice personnel'
+    if @exercice.save!
+      redirect_to exercices_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @exercice.destroy
+    redirect_to exercices_path
+  end
+
+  private
+
+  def set_exercice
+    @exercice = Exercice.find(params[:id])
+  end
+
+  def exercice_params
+    params.require(:exercice).permit(:name, :description)
+  end
 end
