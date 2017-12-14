67f74d9d82f9eb7272fdbf2107b85517dd963335
Romain Capelle <romain.capelle@gmail.com>
2017-12-14 14:04:40 +0100

    sort by in category exercie les notres avant

--- app/javascript/packs/index_seances.js
+++ app/javascript/packs/index_seances.js
@@ -19,37 +19,13 @@ rows.forEach((row) => {
 
 rows.forEach((row) => {
     row.addEventListener('mouseover', (event) => {  
-        row.classList.toggle("row-mouse-hover-effet");
+        console.log("keke");
+        row.classList.add("row-mouse-hover-effet");
     });
 });
 
 rows.forEach((row) => {
     row.addEventListener('mouseout', (event) => {
-        row.classList.toggle("row-mouse-hover-effet");
+        row.classList.remove("row-mouse-hover-effet");
     });
 });
-
-
-
-
-// function linkToSeance() {
-//     const allRows = document.querySelectorAll('.row-seance');
-//     const lastRow = allRows[allRows.length - 1];
-//     lastRow.addEventListener("click", (event) => {
-//         link = lastRow.querySelector('.icon-open-show-seance').parentNode.href;
-//         console.log(link);
-//     });
-// };
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     rows.forEach((row) => {
-//         const allRows = document.querySelectorAll('.row-seance');
-//         const lastRow = allRows[allRows.length - 1];
-//         lastRowLink = lastRow.querySelector('.icon-open-show-seance').parentNode.href;
-//         console.log(lastRowLink);
-//         lastRow.addEventListener('click', (event) => {
-//             console.log(LastrowLink);
-//         });
-//     });
-// });
-
