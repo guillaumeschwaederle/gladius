056163563465ed3d3c9e6b1d956efcc9e51de8e2
Arthur Thieffry <arthieffry@gmail.com>
2017-12-14 01:18:25 +0100

    1er version stat

--- app/controllers/seances_controller.rb
+++ app/controllers/seances_controller.rb
@@ -5,6 +5,10 @@ class SeancesController < ApplicationController
 
   def index
     @seances = Seance.all
+    @stat_trainings = trainings_completion
+    @stat_seances = seances_completion
+    @nb_seances = nb_seances
+    @all_goal = all_goal
   end
 
   def show
@@ -26,5 +30,41 @@ class SeancesController < ApplicationController
 
   def set_training
     @training = Training.find(params[:training_id])
+  end
+
+  def trainings_completion
+    arr = []
+    current_user.profile.trainings.each do |training|
+      arr << training.percent
+    end
+    (arr.sum / arr.count * 100).round()
+  end
+
+  def seances_completion
+    arr = []
+    current_user.profile.trainings.each do |training|
+      training.seances.each do |seance|
+        arr << seance.percent
+      end
+    end
+    (arr.sum / arr.count * 100).round()
+  end
+
+  def nb_seances
+    sum = 0
+    current_user.profile.trainings.each do |training|
+      sum += training.seances.count
+    end
+    sum
+  end
+
+  def all_goal
+    arr = []
+    current_user.profile.trainings.each do |training|
+      training.series.each do |serie|
+        arr << serie.goal
+      end
+    end
+    arr
   end
 end
